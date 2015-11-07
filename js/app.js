6f911e7d9d13fb4cfb80d9c24077f395efab1b64
Andre Garzia <andre@andregarzia.com>
2015-11-07 14:03:07 +0000

    touch thing

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ function init_ui() {
     if( format.canPlayTag ) {
       $("#buttons a").each(
         function() {
-          $(this).click( function() {
+          $(this).bind('touchstart', function() {
+            console.log("touchstart");
             var p = $(this).attr( "data-soundfile");
             window.history.pushState( p, p, "#" + p );
             play_sound( p + "." + format.ext );
